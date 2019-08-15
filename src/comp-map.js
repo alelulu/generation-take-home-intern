f298ea0d7bbc28bcbdc1b471b31481fca217b4d1
alelulu <ale.silvasuazo@gmail.com>
2019-08-15 15:19:59 -0400

    showing all stores as markers on the map

--- src/comp-map.js
+++ src/comp-map.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
-import './comp-map.css'
+import './comp-map.css';
+import stores from './store_directory.json'
 
 class MapGoogle extends Component {
   constructor(props) {
     super(props);
-    this.mapRef = React.createRef(); 
+    this.mapRef = React.createRef();
+    this.state = {
+      markers: [],
+      favorites: []
+    }
   }
 
   componentDidMount() {
     var myLatLng = {lat: 19.4978, lng: -99.1269};
 
     var map = new window.google.maps.Map(this.mapRef.current, {
-      zoom: 9,
+      zoom: 10,
       center: myLatLng
     });
     
-  
-    var marker = new window.google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Hello World!'
+    /* this.getData() */
+    console.log(stores)
+    stores.forEach(element => {
+      let marker = new window.google.maps.Marker({
+        position: element.Coordinates,
+        map: map,
+        title: element.Name
+      });
+      
+      this.setState({
+        markers: this.state.markers.push({
+          position: element.Coordinates,
+          address: element.Address,
+          name: element.Name
+        })
+      })
+      
     });
+    console.log(this.state.markers)
+  
   }
 
   render() {
